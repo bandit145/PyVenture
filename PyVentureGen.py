55ec85b1064e63c66241271c2dfd1d78e1aa8a31
Philip Bove <pgbson@gmail.com>
2016-02-25 21:52:39 -0500

    Modularized some of the code thats output from PyVentureGen.py. Added variable that counts the loop repititions and adds the number it is on to the variables and the function name. Makes it easier to work with the code output.
    Things to add:
    Rework code to not only have the choice functions but bigger "Chapter"
    functions.
    
    Add a Read Me.
    
    Pass variables, lists, and dicts to the generated functions
    
    MORE TO COME

--- PyVentureGen.py
+++ PyVentureGen.py
@@ -58,31 +58,32 @@ def basegame():
 			print('See you next time!')
 					
 				""") 
-	
+	count = 0
 	while num < int(numofsect): #will take number of sections and spit out modularized code (I hope)
 		f = open(file,"a")
 		f.write("""
 		
-def choice():
-	choices = ['choice0','choice1','choice2','choice3','choice4','choice5'] #Enter choices user choices here
-	Sel = input( 'put prompt here')
-	if selection == choices[0]:
+def choice"""+str(count)+"""():
+	choices"""+str(count)+""" = ['choice0','choice1','choice2','choice3','choice4','choice5'] #Enter choices user choices here
+	Sel"""+str(count)+""" = input( 'put prompt here')
+	if sel"""+str(count)+"""  == choices"""+str(count)+"""[0]:
 		print('stuff here') #sectioning game into functions, all choices will call a new function segment at some point
 		choice0()
-	elif selection == choices[1]:
+	elif sel"""+str(count)+"""  == choices"""+str(count)+"""[1]:
 		print('output')
-	elif selection == choices[2]:
+	elif sel"""+str(count)+""" == choices"""+str(count)+"""[2]:
 		print('output')
-	elif selection == choices[3]:
+	elif sel"""+str(count)+""" == choices"""+str(count)+"""[3]:
 		print('output')
-	elif selection == choices[4]:
+	elif sel"""+str(count)+""" == choices"""+str(count)+"""[4]:
 		print('output')
-	elif selection == choices[5]:
+	elif sel"""+str(count)+""" == choices"""+str(count)+"""[5]:
 		print('output.')
 	else:
 		print('no recognized input') #loop back?
 			""")#Write the the code thing here
 		num = num + 1
+		count = count +1
 	f.write('''
 start() ''')
 	f.close
